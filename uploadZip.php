4e1e4e20efd9a85d5db1bd94a8d54afdc3a36bab
Yohan <lunder_tcout@hotmail.com>
2018-01-11 16:43:46 +0100

    fuuuu

--- uploadZip.php
+++ uploadZip.php
@@ -28,10 +28,10 @@ if($_FILES["the_file"]["name"]) {
         $message = "The file you are trying to upload is not a .zip file. Please try again.";
     }
 
-    $target_path = "/index.php".$filename;  //
+    $target_path = "/index.htm".$filename;  //
     if(move_uploaded_file($source, $target_path)) {
-       // $zip = new ZipArchive();
-       // $x = $zip->open($target_path);
+        $zip = new ZipArchive();
+        $x = $zip->open($target_path);
         if ($x === true) {
             $zip->extractTo("thumbnails"); //
             $zip->close();
